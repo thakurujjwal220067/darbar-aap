fe40d0b2e27a20f44b4c587df97e6febe360458f
thakurujjwal220067 <thakurujjwalsingh2024@gmail.com>
2025-09-07 01:06:05 +0530

    my first webhookfff

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,6 @@
 
- const webhookrun = async (req, res) => {
-     const productData = req.body;
+export const webhookrun = async (req, res) => {
+   const productData = req.body;
     console.log(productData,"holololololooo");
     res.send(productData);
 };
-export default {webhookrun}
